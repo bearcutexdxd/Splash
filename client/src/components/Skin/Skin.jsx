c9cc75fb9996c0ca9ecb9d183cdc1b39dff88eac
Sinfulk <sulways95@icloud.com>
2022-08-03 18:11:53 +0300

    shop

--- client/src/components/Skin/Skin.jsx
+++ client/src/components/Skin/Skin.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { hostFrontSkin } from '../../config/endPoints';
-import { getUserSkinsThunk } from '../../redux/actions/skinsAction';
+import { postSkinThunk } from '../../redux/actions/skinsAction';
 
-function Skin(name, id, img) {
+function Skin({ name, skinId, img }) {
   const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
   function SkinIdHandler() {
-    dispatch(getUserSkinsThunk(user, 2));
+    dispatch(postSkinThunk(user, skinId));
   }
   const nameCard = 'pipo-nekonin010.png';
 
   return (
     <div className="card m-4 shop">
-      <img src={`${hostFrontSkin()}${nameCard}`} alt="BEST" />
+      <img src={`${hostFrontSkin()}${img}`} alt="BEST" />
       <div className="card-body">
-        <h5 className="card-title justify-center text-success">Name of Skin</h5>
+        <h5 className="card-title justify-center text-success">{name}</h5>
         <p className="text-info text-2xl">Уникальный скин в игре Kitty Splash</p>
         <div className="card-actions justify-center">
           <button
